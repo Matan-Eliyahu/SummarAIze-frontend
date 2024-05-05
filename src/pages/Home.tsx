54eefc91abc38f5b3895406bf2c44e442429aae9
yuv99999 <yuv99999@gmail.com>
2024-05-05 21:48:53 +0300

    add sign up and expand home styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,12 @@ import pdfIcon from "../assets/pdf-icon.png";
 import docIcon from "../assets/doc-icon.png";
 import audoiIcon from "../assets/audio-icon.png";
 import styles from "../styles/pages/Home.module.scss";
+import styles2 from "../styles/pages/Signup.module.scss";
 import { FcGoogle } from "react-icons/fc";
 import { MdFacebook } from "react-icons/md";
 
 function Home() {
   const navigate = useNavigate();
-
-  function handleButtonClick() {
-    navigate("signup");
-  }
 
   function handleButton2Click() {
     navigate("summarize");
@@ -35,20 +32,42 @@ function Home() {
       </div>
 
       <div className={styles.buttonBox}>
+        <form className={styles2.form}>
+          <label>
+            Email
+            <input type="email" name="email" className={styles2.input} />
+          </label>
+          <label>
+            Password
+            <input type="password" name="password" className={styles2.input} />
+          </label>
+        </form>
+
+        <button className={styles.signinButton} onClick={handleButton2Click}>
+          Sign in
+        </button>
+
+        <div className={styles.boxSeparator}>
+          <hr className={styles.boxSeparatorItem}></hr>
+          <span className={styles.boxSeparatorItem}>or sign in with</span>
+          <hr className={styles.boxSeparatorItem}></hr>
+        </div>
+
         <button className={styles.googleButton} onClick={handleButton2Click}>
           <FcGoogle size={25} />
           Continue with Google
         </button>
         <button className={styles.facebookButton} onClick={handleButton2Click}>
-            <MdFacebook size={25} className={styles.facebookIcon} />
+          <MdFacebook size={25} className={styles.facebookIcon} />
           Continue with Facebook
         </button>
         <button className={styles.appleButton} onClick={handleButton2Click}>
           Continue with Apple
         </button>
-        <button className={styles.signupButton} onClick={handleButtonClick}>
-          Sign up
-        </button>
+
+        <div className={styles.descriptionText}>Don't have an account yet?</div>
+        <a href="/signup">Register now</a>
+
       </div>
 
       <div className={styles.descriptionText}>Start summarizing documents, photos, and recordings effortlessly. Experience the power of AI-driven summaries tailored to your needs</div>
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,10 +1,66 @@
 
+import { useNavigate } from "react-router-dom";
+import logo from "../assets/logo-large.png";
+import pdfIcon from "../assets/pdf-icon.png";
+import docIcon from "../assets/doc-icon.png";
+import audoiIcon from "../assets/audio-icon.png";
+import styles from "../styles/pages/Home.module.scss";
+import styles2 from "../styles/pages/Signup.module.scss";
+
+
 function Signup() {
+
+  const navigate = useNavigate();
+
+  function handleButtonClick() {
+    //vlidations and send to server 
+    navigate("summarize");
+  }
+
   return (
-    <>
-    <div className="signup">Signup</div>
-    </>
-  )
+    <div className="signup">
+      <div className={styles.welcomeBox}>
+        <div className={styles.welcomeText}>Welcome to</div>
+        <img src={logo} alt="logo" style={{ width: "370px" }} />
+      </div>
+
+      <div className={styles.catchwordBox}>
+        <div className={styles.catchwordText}>Transforming Text with AI</div>
+        <div className={styles.iconBox}>
+          <img src={pdfIcon} alt="pdf-icon" className={styles.icon} />
+          <img src={docIcon} alt="doc-icon" className={styles.icon} />
+          <img src={audoiIcon} alt="audio-icon" className={styles.icon} />
+        </div>
+      </div>
+
+      <div className={styles.buttonBox}>
+        <form className={styles2.form}>
+          <label>
+          Username
+            <input type="text" name="username" className={styles2.input} />
+          </label>
+          <label>
+          Email
+            <input type="email" name="email" className={styles2.input} />
+          </label>
+          <label>
+          Password
+            <input type="password" name="password" className={styles2.input} />
+          </label>
+          <label>
+          Confirm Password
+            <input type="password" name="confirmPassword" className={styles2.input} />
+          </label>
+        </form>
+
+          <button className={styles.signupButton} onClick={handleButtonClick}>
+            Sign up
+          </button>
+          <div className={styles.descriptionText}>already have an account?</div>
+          <a href="/">Sign in</a>
+      </div>        
+    </div>
+  );
 }
 
 export default Signup
